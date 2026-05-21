0b63729ad449bfc29048897141e7a355aafc0fa2
MFahadAfzal <fahad.afzal780@gmail.com>
2026-05-21 01:32:17 -0600

    added tests and fix previous tests

--- tests/url.spec.js
+++ tests/url.spec.js
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
 
 //intial test to see if a shortened url will generate
 test('get started link', async ({ page }) => {
-  await page.goto(`${process.env.BASE_URL}`);
+  await page.goto(`${process.env.FRONTEND_URL}`);
 
-  await page.getByRole('textbox').fill('google.com')
+  await page.getByPlaceholder('https://example.com/your/very/long/url').fill('google.com')
 
   await page.getByRole('button', { name: 'Shorten URL' }).click();
 
@@ -18,18 +20,12 @@ test('get started link', async ({ page }) => {
 });
 
 //test to see if an incorrect input into url shortener will return proper error
-test('get started link', async ({ page }) => {
-  await page.goto(`${process.env.BASE_URL}`);
+test('invalid long url', async ({ page }) => {
+  await page.goto(`${process.env.FRONTEND_URL}`);
 
-  await page.getByRole('textbox').fill('this is not a url')
+  await page.getByPlaceholder('https://example.com/your/very/long/url').fill('this is not a url')
 
   await page.getByRole('button', { name: 'Shorten URL' }).click();
 
-  await expect(page.getByText('Nothing Yet')).not.toBeVisible()
-
-  const shortUrl = await page.getByTestId('short-url').textContent()
-  
-  await page.goto(shortUrl)
-
-  await expect(page).toHaveTitle(/Google/);
+  await expect(page.getByText('Please enter a URL')).toBeVisible()
 });
