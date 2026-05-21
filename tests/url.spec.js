9a2675729e42b37d74a3677e30bf7e0264d9e1ba
MFahadAfzal <fahad.afzal780@gmail.com>
2026-05-21 01:38:12 -0600

    added test to chck if creating a short url with an expiration date is possible

--- tests/url.spec.js
+++ tests/url.spec.js
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test';
-import dotenv from 'dotenv';
-dotenv.config();
+import { test, expect } from '@playwright/test'
+import dotenv from 'dotenv'
+dotenv.config()
 
 //intial test to see if a shortened url will generate
 test('get started link', async ({ page }) => {
-  await page.goto(`${process.env.FRONTEND_URL}`);
+  await page.goto(`${process.env.FRONTEND_URL}`)
 
   await page.getByPlaceholder('https://example.com/your/very/long/url').fill('google.com')
 
-  await page.getByRole('button', { name: 'Shorten URL' }).click();
+  await page.getByRole('button', { name: 'Shorten URL' }).click()
 
   await expect(page.getByText('Nothing Yet')).not.toBeVisible()
 
@@ -16,7 +16,26 @@ test('get started link', async ({ page }) => {
   
   await page.goto(shortUrl)
 
-  await expect(page).toHaveTitle(/Google/);
+  await expect(page).toHaveTitle(/Google/)
+});
+
+//testing to see if a url with an expiration date can be made
+test('create short url with expiration', async ({ page }) => {
+  await page.goto(`${process.env.FRONTEND_URL}`);
+
+  await page.getByPlaceholder('https://example.com/your/very/long/url').fill('google.com')
+
+  await page.locator('input[type="date"]').fill('2026-12-31')
+
+  await page.getByRole('button', { name: 'Shorten URL' }).click()
+
+  await expect(page.getByText('Nothing Yet')).not.toBeVisible()
+
+  const shortUrl = await page.getByTestId('short-url').textContent()
+
+  await page.goto(shortUrl)
+
+  await expect(page).toHaveTitle(/Google/)
 });
 
 //test to see if an incorrect input into url shortener will return proper error
@@ -24,8 +43,8 @@ test('invalid long url', async ({ page }) => {
   await page.goto(`${process.env.FRONTEND_URL}`);
 
   await page.getByPlaceholder('https://example.com/your/very/long/url').fill('this is not a url')
-
-  await page.getByRole('button', { name: 'Shorten URL' }).click();
+  
+  await page.getByRole('button', { name: 'Shorten URL' }).click()
 
   await expect(page.getByText('Please enter a URL')).toBeVisible()
 });
