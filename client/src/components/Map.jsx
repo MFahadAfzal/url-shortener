5b63d13d99b758e91927919013de482739110914
MFahadAfzal <fahad.afzal780@gmail.com>
2026-05-21 00:57:55 -0600

    added icons to markers for geolocation

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,11 +1,22 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 
 function Map({clicksData}) {
     const position = [51.505, -0.09]
 
     const clicks = clicksData.filter(click => click.lat !== null)
 
+    
+
+    let DefaultIcon = L.icon({
+        iconUrl: '/icons/marker-icon.png',
+        shadowUrl: '/icons/marker-shadow.png',
+        iconAnchor: [12, 41]
+    })
+
+    L.Marker.mergeOptions({ icon: DefaultIcon })
+    
     return (
         <div className="h-[400px] w-full">
             <MapContainer center={[51.505, -0.09]} zoom={3} style={{ height: '100%', width: '100%' }}>
